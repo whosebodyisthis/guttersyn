1a4731560fce51f8489332b383cec8a9108c0008
whose <133358060+whosebodyisthis@users.noreply.github.com>
2023-09-15 20:08:24 +0100

    Add files via upload

---
+++ guttersyn.lua
@@ -0,0 +1,210 @@
+--
+-- GutterSynthesis
+-- 
+-- norns wrapper for Tom Mudd's
+-- GutterSynthesis
+-- 
+-- K1 -
+-- K2 - Switch control pages
+-- K3 - Switch control params
+--
+-- E1/E2/E3 - Adjust respective
+-- control
+--
+-- Originally by Tom Mudd in
+-- Max/MSP / Java
+--
+-- SuperCollider implementation
+-- by Mads Kjeldgaard and
+-- Scott Carver
+
+local UI = require "ui"
+engine.name = 'GutterSynthesis'
+
+function init()
+  params:add_separator()
+  params:add_control("amp", "amp", controlspec.new(0, 4, "lin", 0.1, 1))
+  params:set_action("amp", function(v) engine.amp(v) end)
+  
+  params:add_separator()
+  params:add_control("mod", "mod", controlspec.new(0, 10, "lin", 0.1, 0.1))
+  params:set_action("mod", function(v) engine.mod(v) end)
+  
+  params:add_control("omega", "omega", controlspec.new(0, 1, "lin", 0.01, 0.01))
+  params:set_action("omega", function(v) engine.omega(v) end)
+  
+  params:add_control("damp", "damp", controlspec.new(0, 1, "lin", 0.01, 0.01))
+  params:set_action("damp", function(v) engine.damp(v) end)
+  
+  params:add_control("rate", "rate", controlspec.new(0.01, 5, "exp", 0.01, 0.3))
+  params:set_action("rate", function(v) engine.rate(v) end)
+  
+  params:add_control("gain", "gain", controlspec.new(0, 3.5, "lin", 0.1, 1))
+  params:set_action("gain", function(v) engine.gain(v) end)
+  
+  params:add_control("smoothing", "smoothing", controlspec.new(0, 5, "lin", 0.1, 0.1))
+  params:set_action("smoothing", function(v) engine.smoothing(v) end)
+  
+  params:add_separator("filter bank settings")
+  params:add_control("pitch", "pitch", controlspec.new(0, 5, "lin", 0.01, 1))
+  params:set_action("pitch", function(v) engine.pitch(v) end)
+  
+  params:add_control("q", "q", controlspec.new(10, 800, "lin", 10, 30))
+  params:set_action("q", function(v) engine.q(v) end)
+  
+  params:add_control("scale1", "bank 1 scale", controlspec.new(0, 9, "lin", 1, 0))
+  params:set_action("scale1", function(v) engine.scale1(v) end)
+  
+  params:add_control("scale2", "bank 2 scale", controlspec.new(0, 9, "lin", 1, 0))
+  params:set_action("scale2", function(v) engine.scale2(v) end)
+  
+  params:add_control("gain1", "gain1", controlspec.new(0, 2.5, "lin", 0.1, 1))
+  params:set_action("gain1", function(v) engine.gain1(v) end)
+  
+  params:add_control("gain2", "gain2", controlspec.new(0, 2.5, "lin", 0.1, 1))
+  params:set_action("gain2", function(v) engine.gain2(v) end)
+  params:bang()
+  
+  -- track current page (out of 2)
+  page = 0
+  -- track block on current page (out of 3)
+  block = 0
+  -- scale names
+  scales = {"bark","12tet","24tet","maj","min","hMaj","hMin","p_o1","p_u1","dim"}
+end
+
+function redraw()
+  screen.clear()
+  screen.level(15)
+  
+  screen.font_face(2)
+  screen.move(0,7)
+  screen.text("GutterSynthesis")
+  
+  screen.line_width(1)
+  screen.move(55,7)
+  screen.line(126,7)
+  screen.close()
+  screen.stroke()
+  
+  --screen.font_face(0)
+  if page == 0 then
+     --screen.move(0,7)
+     --screen.text("amp: " .. params:get("amp"))
+     
+     screen.move(4,17)
+     screen.text("mod")
+     UI.Slider.new(5,22,5,33,params:get("mod"),0,10,{0}):redraw()
+     
+     screen.move(24,17)
+     screen.text("omg")
+     UI.Slider.new(25,22,5,33,params:get("omega"),0,1,{0}):redraw()
+  
+     screen.move(44,17)
+     screen.text("dmp")
+     UI.Slider.new(45,22,5,33,params:get("damp"),0,1,{0}):redraw()
+  
+     screen.move(64,17)
+     screen.text("rate")
+     UI.Slider.new(65,22,5,33,params:get("rate"),0,5,{0}):redraw()
+  
+     screen.move(84,17)
+     screen.text("gain")
+     UI.Slider.new(85,22,5,33,params:get("gain"),0,3.5,{0}):redraw()
+  
+     screen.move(104,17)
+     screen.text("smooth")
+     UI.Slider.new(105,22,5,33,params:get("smoothing"),0,5,{0}):redraw()
+  else
+     --screen.move(0,7)
+     --screen.text("amp: " .. params:get("amp"))
+     
+     screen.move(4,17)
+     screen.text("pitch")
+     UI.Slider.new(5,22,5,33,params:get("pitch"),0,3,{0}):redraw()
+     
+     screen.move(20,30)
+     screen.text("SCALE:")
+     
+     screen.move(20,42)
+     screen.text("1. " .. scales[params:get("scale1")+1])
+     
+     screen.move(20,52)
+     screen.text("2. " .. scales[params:get("scale2")+1])
+  
+     screen.move(64,17)
+     screen.text("Q")
+     UI.Slider.new(65,22,5,33,params:get("q"),10,800,{0}):redraw()
+  
+     --screen.move(84,19)
+     --screen.text("gain1")
+     UI.Dial.new(89,14,10,params:get("gain1"),0,2.5,0.01,0,{},"gain1"):redraw()
+  
+     --screen.move(104,19)
+     --screen.text("gain2")
+     UI.Dial.new(89,36,10,params:get("gain2"),0,2.5,0.01,0,{},"gain2"):redraw()
+  end
+  
+  -- line at bottom indicates active controls
+  if block == 0 then
+    screen.line_width(1)
+    screen.move(0,60)
+    screen.line(56,60)
+    screen.close()
+    screen.stroke()
+  else
+    screen.line_width(1)
+    screen.move(59,60)
+    screen.line(116,60)
+    screen.close()
+   screen.stroke()
+  end
+  
+  screen.update()
+end
+
+function key(n,z)
+  local a = n
+  if n == 2 and z == 1 then
+    page = (page + 1) % 2 
+    block = 0
+  elseif n == 3 and z == 1 then
+    block = (block + 1) % 2
+  end
+  redraw()
+  return(n)
+end
+
+function enc(n,d)
+  
+  if n == 1 and page == 0 and block == 0 then
+    params:delta("mod",d)
+  elseif n == 1 and page == 0 and block == 1 then
+    params:delta("rate",d)
+  elseif n == 1 and page == 1 and block == 0 then
+    params:delta("pitch",d)
+  elseif n == 1 and page == 1 and block == 1 then
+    params:delta("q",d)
+  end
+  
+  if n == 2 and page == 0 and block == 0 then
+    params:delta("omega",d)
+  elseif n == 2 and page == 0 and block == 1 then
+    params:delta("gain",d)
+  elseif n == 2 and page == 1 and block == 0 then
+    params:delta("scale1",d)
+  elseif n == 2 and page == 1 and block == 1 then
+    params:delta("gain1",d)
+  end
+  
+  if n == 3 and page == 0 and block == 0 then
+    params:delta("damp",d)
+  elseif n == 3 and page == 0 and block == 1 then
+    params:delta("smoothing",d)
+  elseif n == 3 and page == 1 and block == 0 then
+    params:delta("scale2",d)
+  elseif n == 3 and page == 1 and block == 1 then
+    params:delta("gain2",d)
+  end
+  redraw()
+end
---
+++ lib/Engine_GutterSynthesis.sc
@@ -0,0 +1,136 @@
+Engine_GutterSynthesis : CroneEngine {
+	// Define a getter for the synth variable
+	var <synth;
+
+	// Define a class method when an object is created
+	*new { arg context, doneCallback;
+		// Return the object from the superclass (CroneEngine) .new method
+		^super.new(context, doneCallback);
+	}
+	alloc {
+		// Define the synth variable, whichis a function
+		SynthDef(\testgen, { arg inL, inR, out, amp, mod, omega, damp, rate, gain, smoothing, q, pitch, scale1, scale2, gain1, gain2;
+			var freq1, freq2, snd1, snd2, scale, enableaudio=0;
+
+			scale = [
+				[50, 150, 250, 350, 455, 570, 700, 925, 1175, 1375, 1600, 1860, 2160, 2510, 2925, 3425, 4050, 4850, 5850, 7050, 8600, 10750, 13500], //Bark scale
+				Array.fill(24,{ arg i; Scale.chromatic.degreeToFreq(i, 45.midicps, 0); }),
+				Array.fill(24,{ arg i; Scale.chromatic24.degreeToFreq(i, 45.midicps, 0); }),
+				Array.fill(24,{ arg i; Scale.major.degreeToFreq(i, 45.midicps, 0); }),
+				Array.fill(24,{ arg i; Scale.minor.degreeToFreq(i, 45.midicps, 0); }),
+				Array.fill(24,{ arg i; Scale.harmonicMajor.degreeToFreq(i, 45.midicps, 0); }),
+				Array.fill(24,{ arg i; Scale.harmonicMinor.degreeToFreq(i, 45.midicps, 0); }),
+				Array.fill(24,{ arg i; Scale.partch_o1.degreeToFreq(i, 45.midicps, 0); }),
+				Array.fill(24,{ arg i; Scale.partch_u1.degreeToFreq(i, 45.midicps, 0); }),
+				Array.fill(24,{ arg i; Scale.diminished.degreeToFreq(i, 45.midicps, 0); }),
+			];
+
+			freq1 = Lag3.kr(pitch * Select.kr(scale1, scale)); //Pitch 0.05 - 2.0
+			freq2 = Lag3.kr(pitch * Select.kr(scale2, scale));
+
+			snd1 = GutterSynth.ar(
+				gamma:         Lag.kr(mod),
+				omega:         Lag3.kr(omega,0.2),
+				c:             Lag.kr(damp),
+				dt:         Lag3.kr(rate,0.2),
+				singlegain: Lag.kr(gain),
+				smoothing:  Lag.kr(smoothing),
+				togglefilters: 1,
+				distortionmethod: 1,
+				oversampling: 2,
+				enableaudioinput: 0,
+				audioinput: inL,
+				gains1:     Lag.kr(gain1),
+				gains2:     Lag.kr(gain2),
+				freqs1:     `freq1,
+				qs1:         Array.rand(24,0.95,1) * q,
+				freqs2:     `freq2,
+				qs2:         Array.rand(24,0.95,1) * q
+			);
+
+			snd2 = GutterSynth.ar(
+				gamma:         Lag.kr(mod),
+				omega:         Lag3.kr(omega,0.2),
+				c:             Lag.kr(damp),
+				dt:         Lag3.kr(rate,0.2),
+				singlegain: Lag.kr(gain),
+				smoothing:  Lag.kr(smoothing),
+				togglefilters: 1,
+				distortionmethod: 1,
+				oversampling: 2,
+				enableaudioinput: 0,
+				audioinput: inR,
+				gains1:     Lag.kr(gain1),
+				gains2:     Lag.kr(gain2),
+				freqs1:     `freq1,
+				qs1:         Array.rand(24,0.95,1) * q,
+				freqs2:     `freq2,
+				qs2:         Array.rand(24,0.95,1) * q
+			);
+
+			Out.ar(out,Balance2.ar(snd1,snd2,0,amp));
+		}).add;
+
+		context.server.sync;
+
+		synth = Synth.new(\testgen, [\inL, context.in_b[0].index, \inR, context.in_b[1].index, \out, context.out_b.index], context.xg);
+
+		// Export argument symbols as modulatable paramaters
+		// This could be extended to control the Lag time as additional params
+		this.addCommand("amp", "f", { arg msg;
+			synth.set(\amp, msg[1]);
+		});
+
+		this.addCommand("mod", "f", { arg msg;
+			synth.set(\mod, msg[1]);
+		});
+
+		this.addCommand("omega", "f", { arg msg;
+			synth.set(\omega, msg[1]);
+		});
+
+		this.addCommand("damp", "f", { arg msg;
+			synth.set(\damp, msg[1]);
+		});
+
+		this.addCommand("rate", "f", { arg msg;
+			synth.set(\rate, msg[1]);
+		});
+
+		this.addCommand("gain", "f", { arg msg;
+			synth.set(\gain, msg[1]);
+		});
+
+		this.addCommand("smoothing", "f", { arg msg;
+			synth.set(\smoothing, msg[1]);
+		});
+
+		this.addCommand("q", "f", { arg msg;
+			synth.set(\q, msg[1]);
+		});
+
+		this.addCommand("pitch", "f", { arg msg;
+			synth.set(\pitch, msg[1]);
+		});
+
+		this.addCommand("scale1", "f", { arg msg;
+			synth.set(\scale1, msg[1]);
+		});
+
+		this.addCommand("scale2", "f", { arg msg;
+			synth.set(\scale2, msg[1]);
+		});
+
+		this.addCommand("gain1", "f", { arg msg;
+			synth.set(\gain1, msg[1]);
+		});
+
+		this.addCommand("gain2", "f", { arg msg;
+			synth.set(\gain2, msg[1]);
+		});
+	}
+	// define a function that is called when the synth is shut down
+	free {
+		synth.free;
+	}
+}
